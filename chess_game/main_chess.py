c57dda14bd7e9d8aa63a2a165c3fa89273b8a94e
Rachel Newman <128507881+rachek253@users.noreply.github.com>
2026-04-06 01:41:36 -0700

    Update main_chess.py

---
+++ chess_game/main_chess.py
@@ -0,0 +1,13 @@
+"""
+File name: main_chess.py
+
+Author(s):
+
+Purpose: Main file to execute chess engine.
+All necessary librarys will be loaded here.
+
+Please add code as we get it! I will work on
+creating the libraries for each of the main
+classes for the chess engine as we make files for them! - Rachel
+"""
+# insert main file code here
