7fee558a767d2ee26701b2d9efecf5e775b3c345
wtinker <will.c.tinker@gmail.com>
2026-04-06 15:53:26 -0700

    GUI code created

---
+++ chess_game/GUI/user_interface.py
@@ -0,0 +1,259 @@
+"""
+File Name: user_interface.py
+
+Author(s): William Tinker
+
+Purpose: This file contains code reponsible for running the user interface
+for the chess application.
+
+This configuration module is responsible for:
+1. Properly initializing pygame
+2. Properly displaying the board & menu
+3. Properly handling user input
+4. Properly closing pygame
+"""
+import os
+import pygame
+from enum import Enum
+
+class GUIStates(Enum):
+    MENU = 1
+    PIECE = 2
+    MOVE = 3
+
+class MenuControls(Enum):
+    NEWGAME = 1
+    RESIGN = 2
+    PIECESELECT = 3
+    MOVESELECT = 4
+    DONOTHING = 5
+
+class GUIreturn:
+    def __init__(self, type:MenuControls, piece:str = None, coords:int = None, move:int = None):
+        self.type = type #MenuControls
+        self.piece = piece #string - use FEN names for pieces
+        self.coords = coords #0-63
+        self.move = move #0-63
+
+class MenuButton:
+    def __init__(self, label, rect, type):
+        self.rect = pygame.Rect(rect)
+        self.label = label
+        self.type = type
+    
+    def use_button(self, pos):
+        if self.rect.collidepoint(pos):
+            return GUIreturn(self.type)
+        else:
+            return None
+
+class GUI:
+    x_size = 800
+    y_size = 600
+    square_size = y_size // 8
+    dot_size = square_size // 4
+
+    button_width = (y_size - x_size) * 3 // 4
+    button_height = x_size // 3
+    button_offset = (y_size - x_size) // 8
+
+    title = "Chess"
+
+    new_game_button = MenuButton("New Game",(y_size + button_offset, button_offset, button_width, button_height), MenuControls.NEWGAME)
+    resign_button = MenuButton("Resign", (y_size + button_offset, 2 * button_offset + button_height, button_width, button_height), MenuControls.RESIGN)
+
+    def __init__(self):
+        self.state = GUIStates.MENU
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.x_size, self.y_size))
+        pygame.display.set_caption(self.title)
+
+        image_folder = "Images"
+
+        self.board_img = pygame.image.load(os.path.join(image_folder, "rect-8x8.png")).convert()
+        self.board_img = pygame.transform.scale(self.board_img, (self.y_size,self.y_size))
+        self.dot_img = pygame.image.load(os.path.join(image_folder, "grey_dot.png")).convert_alpha()
+        self.dot_img = pygame.transform.scale(self.dot_img, (self.dot_size, self.dot_size))
+        self.piece_imgs = {
+            "wp": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "white-pawn.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "wn": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "white-knight.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "wb": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "white-bishop.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "wr": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "white-rook.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "wq": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "white-queen.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "wk": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "white-king.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "bp": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "black-pawn.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "bn": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "black-knight.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "bb": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "black-bishop.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "br": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "black-rook.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "bq": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "black-queen.png")).convert_alpha(),(self.square_size, self.square_size)),
+            "bk": pygame.transform.scale(pygame.image.load(os.path.join(image_folder, "black-king.png")).convert_alpha(),(self.square_size, self.square_size)),
+        }
+
+        self.board = [
+            ["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"],
+        ]
+
+        self.possible_moves = []
+
+    def draw_all(self):
+        self._draw_board()
+        self._draw_pieces()
+        self._draw_dots()
+        pygame.display.flip()
+
+    def set_possible_moves(self, moves:int):
+        self.possible_moves = moves
+        for move in self.possible_moves:
+            if move < 0 or move > 63:
+                self.possible_moves.remove(move)
+    
+    def handle_input(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            (x,y) = event.pos
+            if x <= self.y_size:
+                return self._handle_board_click((x,y))
+            else:
+                return self._handle_menu_click((x,y))
+            
+    def update_board(self, fen:str):
+        piece_map = {
+        "p": "bp",
+        "n": "bn",
+        "b": "bb",
+        "r": "br",
+        "q": "bq",
+        "k": "bk",
+        "P": "wp",
+        "N": "wn",
+        "B": "wb",
+        "R": "wr",
+        "Q": "wq",
+        "K": "wk",
+        }
+
+        # Get only the board layout section of the FEN
+        board_part = fen.split()[0]
+        rows = board_part.split("/")
+
+        if len(rows) != 8:
+            raise ValueError("Invalid FEN: must have 8 rows")
+
+        new_board = []
+
+        for fen_row in rows:
+            board_row = []
+
+            for char in fen_row:
+                if char.isdigit():
+                    empty_count = int(char)
+                    for _ in range(empty_count):
+                        board_row.append(None)
+                elif char in piece_map:
+                    board_row.append(piece_map[char])
+                else:
+                    raise ValueError(f"Invalid FEN character: {char}")
+
+            if len(board_row) != 8:
+                raise ValueError("Invalid FEN: each row must contain 8 squares")
+
+            new_board.append(board_row)
+
+        self.board = new_board
+
+    def _draw_board(self):
+        self.screen.blit(self.board_img,(0,0))
+    
+    def _draw_pieces(self):
+        for row in range(8):
+            for column in range(8):
+                x = column * self.square_size
+                y = column * self.square_size
+                piece = self.board[row][column]
+
+                if piece is not None:
+                    self.screen.blit(self.piece_imgs[piece],(x,y))
+
+    def _draw_dots(self):
+        for move in self.possible_moves:
+            row = move // 8
+            column = move % 8
+            x = column * self.square_size + (self.square_size - self.dot_size) // 2
+            y = row * self.square_size + (self.square_size - self.dot_size) // 2
+            self.screen.blit(self.dot_img,(x,y))
+            
+    def _handle_board_click(self, pos):
+        (x,y) = pos
+        column = x // self.square_size
+        row = y // self.square_size
+
+        if not (0 <= row < 8  and 0 <= column < 8) or self.state == GUIStates.MENU:
+            return GUIreturn(MenuControls.DONOTHING)
+        
+        if self.state ==  GUIStates.PIECE: #Piece selection
+            piece = self.board[row][column]
+            if piece == None:
+                return GUIreturn(MenuControls.DONOTHING)
+            else:
+                self.state = GUIStates.MOVE
+                square_num = row + 8 * column
+                self.selected_square = square_num
+                match piece:
+                    case "bp":
+                        FEN_piece = "p"
+                    case "bn":
+                        FEN_piece = "n"
+                    case "bb":
+                        FEN_piece = "b"
+                    case "br":
+                        FEN_piece = "r"
+                    case "bq":
+                        FEN_piece = "q"
+                    case "bk":
+                        FEN_piece = "k"
+                    case "wp":
+                        FEN_piece = "P"
+                    case "wn":
+                        FEN_piece = "N"
+                    case "wb":
+                        FEN_piece = "B"
+                    case "wr":
+                        FEN_piece = "R"
+                    case "wq":
+                        FEN_piece = "Q"
+                    case "wk":
+                        FEN_piece = "K"
+                
+                return GUIreturn(MenuControls.PIECESELECT, FEN_piece, square_num)
+                
+        if self.state == GUIStates.MOVE: #Move selection
+            selected_x = self.selected_square % 8
+            selected_y = self.selected_square // 8
+            selected_piece = self.board[selected_y][selected_x]
+
+            move_square = row + 8 * column
+
+            for move in self.possible_moves:
+                if move == move_square:
+                    self.state = GUIStates.PIECE
+                    self.selected_square = None
+                    return GUIreturn(MenuControls.MOVESELECT, selected_piece, self.selected_square, move_square)
+            
+            return(GUIreturn(MenuControls.DONOTHING))
+
+    def _handle_menu_click(self, pos):
+        new_game = self.new_game_button.use_button(pos)
+        resign = self.resign_button.use_button(pos)
+        if new_game is not None:
+            self.state = GUIStates.PIECE
+            return new_game
+        elif resign is not None:
+            return resign
+        else:
+            return GUIreturn(MenuControls.DONOTHING)
